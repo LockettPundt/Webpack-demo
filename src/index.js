0cceeb2b4210759fb63c16094f341e32a4b70663
Lockett Pundt <kalifornium@hotmail.com>
2020-07-14 21:48:16 -0400

    getting started with setting up webpack.

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import moment from 'moment';
+import './style.css';
+
+const button = document.getElementById('justAButton');
+
+button.addEventListener('click', () => {
+  const timeToAdd = createTime();
+  const timeDiv = document.querySelector('.timeDiv');
+  button.classList.add('button');
+  timeDiv.innerHTML = timeToAdd;
+});
+
+function createTime() {
+  const currentTime = new Date();
+  const humanReadableDate = moment(currentTime).format('LLLL');
+
+  return humanReadableDate;
+}
