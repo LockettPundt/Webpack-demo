cf9de360ed7f42cf90883a610ad842a261fb6150
Lockett Pundt <kalifornium@hotmail.com>
2020-07-15 10:35:11 -0400

    using cleanWebpack plug in to clean up older build files on each build. htmlwebpackplugin to create a new html file in the build on each build. Changed the output to create a bunch for each entry point.

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    app: './src/index.js',
+    printMe: './src/printMe.js',
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-    ],
+  plugins: [
+    // cleans up old build files on each new build.
+    new CleanWebpackPlugin(),
+    // creates a new html file in dist. Currently using src/index.html as template.
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: 'src/index.html',
+    }),
+  ],
+  // outputs the bundles as seperate files based on the entry name.
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
