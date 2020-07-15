f000462ff5b8f42d534f8f6d98f15b8a415d54df
Lockett Pundt <kalifornium@hotmail.com>
2020-07-15 11:34:41 -0400

    imported moment module.

--- src/printMe.js
+++ src/printMe.js
@@ -1,3 +1,9 @@
+import moment from 'moment';
+
 export default function printMe() {
-  console.log('Hi there, How are you?');
+  const time = new Date();
+  console.log(
+    `Hi there, How are you?
+  The current time is ${moment(time).format('LL')}`,
+  );
 }
