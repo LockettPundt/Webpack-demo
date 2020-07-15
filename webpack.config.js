082a1c8dea7d4a0aea64f23f7344cd39cc2c4ad9
Lockett Pundt <kalifornium@hotmail.com>
2020-07-15 10:55:24 -0400

    dev mode added, devtool inline soure map that shows where the errors are happening specifically. devServer to hot reload and watch for changes ( nodemon ).

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  // sets mode to dev.
+  mode: 'development',
   entry: {
     app: './src/index.js',
     printMe: './src/printMe.js',
   },
+  // amazing. shows errors in which file and line.
+  devtool: 'inline-source-map',
+  // points the dev server at the dist directory to watch for changes.
+  devServer: {
+    contentBase: './dist',
+  },
   plugins: [
     // cleans up old build files on each new build.
-    new CleanWebpackPlugin(),
+    // currently not cleaning older ones since we are using dev-server.
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     // creates a new html file in dist. Currently using src/index.html as template.
     new HtmlWebpackPlugin({
       title: 'Output Management',
